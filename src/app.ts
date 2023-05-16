53ec2109824b4d9188ef8ab2fcc9045714ba4cf4
Joabesv <joabevarjao123@gmail.com>
2023-05-16 00:17:33 -0300

    chore(logger): human readable logs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import fastify from 'fastify';
 import { connectToMongo } from './database/connection';
+import { loggerSetup } from './config/logger';
 import usage from './routes/usage';
 
 export async function buildApp() {
   const app = fastify({
-    logger: true,
+    logger: loggerSetup,
   });
 
   try {
