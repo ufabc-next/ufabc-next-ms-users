01ebbc29435d8b12b679c01988303ca83d8fd64c
Joabesv <joabevarjao123@gmail.com>
2023-05-23 00:44:22 -0300

    :sparkles: feat(populate): partially implement insert

--- src/helpers/populate/index.ts
+++ src/helpers/populate/index.ts
@@ -6,31 +6,39 @@ const app = require('../app');
 const bluebird = require('bluebird');
 const cachegoose = require('cachegoose');
 
- // this file populates MongoDB test database with data
+type PopulateOptions = {
+  app: any
+  operation: string
+  whichModels: string
+  readonly COMMUNITY: 'test',
+}
+
+// this file populates MongoDB test database with data
 (async () => {
-    try {
-      await populate();
-      process.exit(0);
-    } catch (e) {
-      console.error(e);
-      process.exit(1);
-    }
+  try {
+    await populate();
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
 })();
 
-
-
-function setInsertPriority(populateFiles: Record<string, string>) {
+function setInsertPriority(
+  populateFiles: Record<string, (ids?: Record<string, unknown>) => Array<unknown>>,
+) {
   const files = Object.keys(populateFiles).reverse();
 
   return zipObj(
     files,
-    files.map(file => {
+    files.map((file) => {
       return populateFiles[file];
     }),
   );
 }
+
 async function populate() {
-  let until;
+  // Figure out what this do
 
   const populateOpts = {
     app,
@@ -38,10 +46,9 @@ async function populate() {
     whichModels: process.argv[3],
     // DO NOT CHANGE THIS
     COMMUNITY: 'test',
-    until,
-  };
+  } satisfies PopulateOptions;
 
-  if (config.NODE_ENV == 'prod') {
+  if (config.NODE_ENV === 'prod') {
     throw new Error('You cannot populate under production mode!!!');
   }
 
@@ -59,129 +66,53 @@ async function populate() {
   }
 
   if (populateOpts.operation === 'delete') {
-    return dumpDatabases(populateOpts);
+    // return dumpDatabases(populateOpts);
   }
 
   if (populateOpts.operation === 'reset') {
-    await dumpDatabases(populateOpts);
+    // await dumpDatabases(populateOpts);
     await createDatabases(populateOpts);
   }
 }
-async function createDatabases({ app, COMMUNITY, whichModels, until }) {
+
+async function createDatabases({ app, COMMUNITY, whichModels }: PopulateOptions) {
   // load models from data folder and sort them by priority
-
-  const data = setInsertPriority(await import('./data'));
-
+  const models = setInsertPriority(await import('./data'));
   // store all the ids for every model
-  let ids = {};
+  const ids = {};
 
-  for (let model in data) {
-    if (whichModels != null && !whichModels.includes(model)) continue;
-    let generateData = data[model];
-
-    // must pass the ids to generateData(ids), because some models need it
-    let dataModels = generatedData(app, ids);
+  for (const model in models) {
+    // Se for passado um modelo, na cli popule e siga em frente
+    if (!whichModels.includes(model)) continue;
+    const generatedData = models[model]
+    const data = generatedData(ids);
 
     // avoid wrong device sign up on dev enviroment
     // this can cause Firebase FCM problems
-    if (app.config.ENV == 'dev' && model == 'user') {
-      dataModels = dataModels.map((m) => {
-        delete m.devices;
-        return m;
+    if (config.NODE_ENV === 'dev' && model === 'user') {
+      data.forEach((model: any) => {
+        // perf wise is better than `delete`
+        model.devices = undefined;
       });
     }
-  
-  
 
-    let Model, resp;
+    // TODO: implement byQuad function and findQuad
+    let Model = models[model].byQuad(quad.findQuadKey)
 
-    Model = app.models[model].bySeason(app.helpers.season.findSeasonKey());
     if (['teachers', 'subjects', 'histories'].includes(model)) {
-      Model = app.models[model];
+      Model = models[model]
     }
 
-    let shouldIndex = Model.schema.plugins.some((p) =>
-      _.get(p.opts, 'indexAutomatically', false)
-    );
+    // don't even know where to start here
+    // const shouldIndex = Model.schema.plugins.some((p) =>
+    //   _.get(p.opts, 'indexAutomatically', false)
+    // );
 
-    // wait for everything being properly indexed in elasticsearch
-    let saved = dataModels.map((data) => {
-      return new Promise(function (resolve) {
-        Model.create(data, (err, model) => {
-          console.log(err);
-          if (err) {
-            throw new Error(err);
-          }
-          if (!shouldIndex) resolve(model);
-          else
-            model.once('es-indexed', () => {
-              resolve(model);
-            });
-        });
-      });
-    });
-
-    resp = await Promise.all(saved);
-
-    // reload indexes
-    if (shouldIndex) {
-      let client = app.elastic;
-      let refresh = bluebird.promisify(client.indices.refresh, {
-        context: client,
-      });
-      let exists = bluebird.promisify(client.indices.exists, {
-        context: client,
-      });
-
-      let indexName = model.toLowerCase();
-
-      // refresh index if exists
-      let indexExists = await exists({ index: indexName });
-      if (indexExists) {
-        await refresh({ index: indexName });
-      }
-    }
-
-    // store response in ids.model
-    ids[model] = resp;
-    if (until != null && until.includes(model)) break;
+    const insertedInDatabase = data.map(value => {
+      return new Promise((resolve, reject) => {
+      })
+    })
   }
-
+  
   return ids;
-}
-
-async function dumpDatabases({ app, COMMUNITY, whichModels, until }) {
-  const data = setInsertPriority(await import('./data'));
-
-  cachegoose.clearCache(null);
-  await app.redis.cache.clear();
-
-  for (let key in data) {
-    if (whichModels != null && !whichModels.includes(key)) continue;
-
-    let Model = app.models[key];
-
-    // dump all indexes for a given model in elasticsearch
-    if (_.isFunction(Model.search)) {
-      let client = app.elastic;
-      let remove = bluebird.promisify(client.indices.delete, {
-        context: client,
-      });
-      let exists = bluebird.promisify(client.indices.exists, {
-        context: client,
-      });
-
-      let indexName = key.toLowerCase();
-
-      // remove index if exists
-      let indexExists = await exists({ index: indexName });
-      if (indexExists) {
-        await remove({ index: indexName });
-      }
-    }
-
-    await app.models[key].remove({});
-
-    if (until != null && until.includes(key)) break;
-  }
 }
