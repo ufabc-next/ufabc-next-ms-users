635e4a89e63576018a99b0eca9456a5fc35db458
Joabesv <joabevarjao123@gmail.com>
2023-05-26 16:14:06 -0300

    :recycle: Populate refac
    remove uselless code, and better organization

--- src/helpers/populate/index.ts
+++ src/helpers/populate/index.ts
@@ -1,16 +1,16 @@
-import { config } from '@/config/env';
-import { zipObj } from 'remeda';
+import { config } from "@/config/env";
+import { zipObj } from "remeda";
 
-const app = require('../app');
-const bluebird = require('bluebird');
-const cachegoose = require('cachegoose');
+// 2 more to remove
+const app = require("../app");
+const cachegoose = require("cachegoose");
 
 type PopulateOptions = {
-  app: any
-  operation: string
-  whichModels: string
-  readonly COMMUNITY: 'test',
-}
+  app: any;
+  operation: string;
+  whichModels: string;
+  readonly COMMUNITY: "test";
+};
 
 // this file populates MongoDB test database with data
 (async () => {
@@ -24,7 +24,10 @@ type PopulateOptions = {
 })();
 
 function setInsertPriority(
-  populateFiles: Record<string, (ids?: Record<string, unknown>) => Array<unknown>>,
+  populateFiles: Record<
+    string,
+    (ids?: Record<string, unknown>) => Array<unknown>
+  >
 ) {
   const files = Object.keys(populateFiles).reverse();
 
@@ -32,7 +35,7 @@ function setInsertPriority(
     files,
     files.map((file) => {
       return populateFiles[file];
-    }),
+    })
   );
 }
 
@@ -44,66 +47,59 @@ async function populate() {
     operation: process.argv[2],
     whichModels: process.argv[3],
     // DO NOT CHANGE THIS
-    COMMUNITY: 'test',
+    COMMUNITY: "test",
   } satisfies PopulateOptions;
 
-  if (config.NODE_ENV === 'prod') {
-    throw new Error('You cannot populate under production mode!!!');
+  if (config.NODE_ENV === "prod") {
+    throw new Error("You cannot populate under production mode!!!");
   }
 
-  if (!['insert', 'delete', 'reset'].includes(populateOpts.operation)) {
-    throw new Error('Wrong operation. Choose between: insert, delete or reset');
+  if (!["insert", "delete", "reset"].includes(populateOpts.operation)) {
+    throw new Error("Wrong operation. Choose between: insert, delete or reset");
   }
 
-  console.info('Bootstrapping necessary components...');
+  console.info("Bootstrapping necessary components...");
   console.info();
   // Preciso prover isso la do outro lado para nao quebrar o populate
-  await app.bootstrap(['config', 'helpers', 'mongo', 'models', 'redis']);
+  await app.bootstrap(["config", "helpers", "mongo", "models", "redis"]);
 
-  if (populateOpts.operation === 'insert') {
+  if (populateOpts.operation === "insert") {
     return createDatabases(populateOpts);
   }
 
-  if (populateOpts.operation === 'delete') {
+  if (populateOpts.operation === "delete") {
     // return dumpDatabases(populateOpts);
   }
 
-  if (populateOpts.operation === 'reset') {
+  if (populateOpts.operation === "reset") {
     // await dumpDatabases(populateOpts);
     await createDatabases(populateOpts);
   }
 }
 
-async function createDatabases({ app, COMMUNITY, whichModels }: PopulateOptions) {
-  // load models from data folder and sort them by priority
-  const data = setInsertPriority(await import('./data'));
-  // store all the ids for every model
-  let ids: Record<string,  {}> = {};
-  
+async function createDatabases({
+  app,
+  COMMUNITY,
+  whichModels,
+}: PopulateOptions) {
+  const data = setInsertPriority(await import("./data"));
   for (const model in data) {
+    const ids: Record<string, unknown> = {};
     if (whichModels != null && !whichModels.includes(model)) {
       continue;
     }
-    const generateData = data[model];
-    const dataModels = generateData(ids);
-    // Objeto com os models a serem inseridos
-    let Model = app.models[model].bySeason(app.helpers.season.findSeasonKey());
-    // acredito que seja pela ordem de dependencia
-    if (['teachers', 'subjects', 'histories'].includes(model)) {
-      Model = app.models[model];
-    }
-    const saved = dataModels.map((data) => {
-      return new Promise(function (resolve, reject) {
-        Model.create(data, (err: unknown, model: {}) => {
-          if (err) return reject(err);
-          // Save to database
-          resolve(model);
-        });
-      });
+    const setupData = data[model];
+    const ModelsValue = setupData(ids);
+    const quad = app.helpers.season.findSeasonKey();
+    const Model = app.models[model].bySeason(quad);
+    const pendingValues = ModelsValue.map(async (value) => {
+      try {
+        return Model.create(value);
+      } catch (error) {
+        throw error;
+      }
     });
-
-    const resp = await Promise.all(saved);
-    ids[model] = resp;
+    const insertedValues = await Promise.all(pendingValues);
+    ids[model] = insertedValues;
   }
-  return ids;
 }
