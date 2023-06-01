8e75bd730936fea1c46d435dfae0644175a6948e
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-05-31 22:36:27 -0300

    fix: adding types to mongoose schema

--- src/model/Student.ts
+++ src/model/Student.ts
@@ -5,7 +5,28 @@ import { findQuarter } from '@/helpers/find-quad';
 type StudentModelType = Model<Student, {}, {}>;
 // TODO: Actually type  the returned user
 
-const studentSchema = new Schema<Student>({});
+const studentSchema = new Schema<Student>({
+  ra: { type: Number, required: true, unique: true },
+  login: { type: String, required: true, unique: true },
+  aluno_id: { type: Number, required: true, unique: true },
+  quad: { type: Number, required: true },
+  year: { type: Number, required: true },
+  quads: { type: Number, required: true },
+  cursos: [
+    {
+      id_curso: { type: Number, required: true },
+      nome_curso: { type: String, required: true },
+      cp: { type: Number, required: true },
+      cr: { type: Number, required: true },
+      ind_afinidade: { type: Number, required: true },
+      turno: {
+        type: String,
+        required: true,
+        enum: ['matutino', 'noturno', 'vespertino'],
+      },
+    },
+  ],
+});
 
 function setQuarter(doc: Student) {
   const quarter = findQuarter();
