0acb2aa474b7061cc868625b430c6cfa41b73bce
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-05-31 22:15:56 -0300

    fix: fix types in zod schema

--- src/model/zod/EnrollmentSchema.ts
+++ src/model/zod/EnrollmentSchema.ts
@@ -1,4 +1,4 @@
-import { ObjectId } from 'mongoose';
+import { Types } from 'mongoose';
 import { z } from 'zod';
 
 const enrollmentSchema = z.object({
@@ -8,19 +8,19 @@ const enrollmentSchema = z.object({
   ra: z.number(),
   disciplina: z.string(),
   subject: z.object({
-    _id: z.custom<ObjectId>(),
+    _id: z.custom<Types.ObjectId>(),
   }),
   campus: z.string(),
   season: z.string(),
   turno: z.union([z.string(), z.string(), z.string()]),
   turma: z.string(),
   teoria: z.object({
-    _id: z.custom<ObjectId>(),
+    _id: z.custom<Types.ObjectId>(),
   }),
   pratica: z.object({
-    _id: z.custom<ObjectId>(),
+    _id: z.custom<Types.ObjectId>(),
   }),
-  mainTeacher: z.custom<ObjectId | {}>(),
+  mainTeacher: z.custom<Types.ObjectId | {}>(),
   comments: z.enum(['teoria', 'pratica']).array(),
   // Vem do portal
   conceito: z.string(),
