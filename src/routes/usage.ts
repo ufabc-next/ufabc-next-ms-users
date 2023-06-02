c374734908f51d4346322c937da82f72a74d45ba
Joabesv <joabevarjao123@gmail.com>
2023-06-01 23:36:25 -0300

    feat(route): prefix the ms with v2

--- src/routes/usage.ts
+++ src/routes/usage.ts
@@ -4,9 +4,9 @@ import type { FastifyPluginAsync } from 'fastify';
 const nextUsageRoute: FastifyPluginAsync = async (fastify, opts) => {
   fastify.get('/', async (request, reply) => {
     try {
-      const users = await getNextUsage();
-      fastify.log.info('Next Usage', users);
-      return reply.code(200).send({ data: users });
+      const usageInfo = await getNextUsage();
+      fastify.log.info({ NextUsage: usageInfo });
+      return reply.code(200).send(usageInfo);
     } catch (error: unknown) {
       fastify.log.error(error, 'error in NextUsageRoute');
       throw error;
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,9 @@ import { config } from '@/config/secret';
 async function start() {
   const app = await buildApp();
   try {
+    if (config.NODE_ENV === 'dev') {
+      app.log.info(`registered routes ${app.printRoutes()}`);
+    }
     app.listen({ port: config.PORT, host: config.HOST });
   } catch (error) {
     app.log.fatal(error, 'error in startup');
